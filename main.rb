0f4ef62deb24b823f50dfe2d7a4f75083736e7f9
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-24 17:21:31 +0800

    Add a new student function.

--- main.rb
+++ main.rb
@@ -1,1 +1,27 @@
-puts "Hello World"
+require_relative 'student'
+
+def add_student
+    id = Student.all.size + 1
+
+    puts "=================================================="
+    puts "CREATING NEW STUDENT"
+
+    print "Enter Full Name: "
+    name = gets.chomp
+
+    print "Birth Date: "
+    birth_date = gets.chomp
+
+    print "Enter Email: "
+    grade = gets.chomp
+
+    print "Enter Phone Number: "
+    address = gets.chomp
+
+    student = Student.new(id, name, birth_date, grade, address)
+    student.save
+
+    puts "=================================================="
+    puts student.display
+    puts "STUDENT ADDED SUCCESSFULLY"
+end
