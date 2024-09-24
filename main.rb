1fbd6122a074c997f33625c33ee82d43c52f5d6d
Angelica Rose Gumanid <84778428+AngelicaGumanid@users.noreply.github.com>
2024-09-25 07:38:16 +0800

    Added a delete student and repeating action using loop.

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 require_relative 'student'
 
-def add_student
-    id = Student.all.size + 1
+# ================================================== STUDENT MANAGEMENT ==================================================
+def add_student # ----- Add student -----
+    student_id = Student.all.size + 1
 
-    puts "=================================================="
-    puts "CREATING NEW STUDENT"
+    puts "\n=================================================="
+    puts "CREATING NEW STUDENT RECORD\n"
 
     print "Enter Full Name: "
     name = gets.chomp
@@ -13,15 +14,71 @@ def add_student
     birth_date = gets.chomp
 
     print "Enter Email: "
-    grade = gets.chomp
+    email = gets.chomp
 
     print "Enter Phone Number: "
-    address = gets.chomp
+    phone_number = gets.chomp
 
-    student = Student.new(id, name, birth_date, grade, address)
+    student = Student.new(student_id, name, birth_date, email, phone_number)
     student.save
 
-    puts "=================================================="
+    puts "==================================================\n\n"
     puts student.display
-    puts "STUDENT ADDED SUCCESSFULLY"
+    puts "\nSTUDENT ADDED SUCCESSFULLY"
+end
+
+def delete_student # ----- Delete student -----
+    puts "\n=================================================="
+    puts "DELETE A STUDENT RECORD\n"
+
+    print "Enter Student ID to delete: "
+    id = gets.chomp.to_i
+    student = Student.find(id)
+
+    if student
+        puts "\nStudent ID: #{student.id}"
+        puts "Name: #{student.name}"
+        puts "Birthdate: #{student.birth_date}"
+        puts "Email: #{student.email}"
+        puts "Phone Number #{student.phone_number}\n"
+        puts "=================================================="
+        print "\nAre you sure you want to delete? (y/n):  "
+        action = gets.chomp
+
+        if action.downcase == 'y'
+            student.destroy
+            puts "\nStudent destroyed successfully.\n"
+        end
+    else
+        puts "\nStudent not found."
+    end
+end
+
+loop do # ----- student management -----
+    system("clear")
+    puts "\n======================================================================"
+    puts "Choose a number to correspond to the action you want:"
+    puts "[1] Add Student Information"
+    puts "[2] Delete Student Information"
+    puts "[3] Exit Student Management"
+    puts "======================================================================\n"
+    print "\nWhat action would you like to do?: "
+    action = gets.chomp
+
+    case action
+    when "1"
+        add_student
+    when "2"
+        delete_student
+    when "3"
+        print "\nAre you sure you want to exit Student Management (y/n)? "
+        action = gets.chomp
+
+        if action.downcase == 'y'
+            puts "\nEXITING STUDENT MANAGEMENT!"
+            break
+        end
+    else
+        puts "Invalid option. Please try again."
+    end
 end
